aaadf2798b3d2aa8b1e0f1d1c6f269cce000c8fb
ellyheetov <yaaajcm@naver.com>
2020-12-13 16:41:53 +0900

    feat(step1) : 밀어내기 함수 구현

--- MastersCodeTest/Computer.swift
+++ MastersCodeTest/Computer.swift
@@ -18,7 +18,19 @@ struct Computer {
     }
     
     private func pushString(input: Input) -> String{
-        return "leapp"
+        let result : String
+        
+        switch input.direct {
+        case .Left :
+            let firstIndex = String.Index(utf16Offset: 0, in: input.str)
+            let lastIndex = String.Index(utf16Offset: input.count, in: input.str)
+            result = String(input.str[lastIndex..<input.str.endIndex]) + String(input.str[firstIndex..<lastIndex])
+        case .Right :
+            let startIndex = String.Index(utf16Offset: 0, in: input.str)
+            let lastIndex = String.Index(utf16Offset: input.str.count - input.count, in: input.str)
+            result = String(input.str[lastIndex..<input.str.endIndex]) + String(input.str[startIndex..<lastIndex])
+        }
+        return result
     }
 
     private func printString(str : String) {

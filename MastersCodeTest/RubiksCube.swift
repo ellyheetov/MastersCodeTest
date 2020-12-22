e55316bc6f2417acc355ec053cf3b5aeb98c2d3e
ellyheetov <yaaajcm@naver.com>
2020-12-22 11:50:38 +0900

    feat(step3) : skeleton code 추가

--- MastersCodeTest/RubiksCube.swift
+++ MastersCodeTest/RubiksCube.swift
@@ -9,14 +9,30 @@ import Foundation
 
 class RubiksCube {
     var cube : [PlaneCube]
+    var continueToPlayGame = true
     
     init() {
         cube = [PlaneCube]()
         for color in CubeColor.allCases {
             cube.append(PlaneCube(color: color))
         }
+        // TODO: shuffle
     }
     
+    func move(inputs: [String]?) {
+        for input in inputs! {
+            let convertedDirect = InputControl.convertedTurn(input: input)
+            
+            if( convertedDirect.0 == .quit){
+                self.continueToPlayGame = false
+                break
+            }
+            pushCube(dirt: convertedDirect)
+        }
+    }
+    func pushCube(dirt: Direct){
+
+    }
     func printCube() {
         for item in cube {
             item.printPlane()
--- MastersCodeTest/main.swift
+++ MastersCodeTest/main.swift
@@ -7,5 +7,19 @@
 
 import Foundation
 
-var cube = RubiksCube()
-cube.printCube()
+struct CubeGame{
+    var cube = RubiksCube()
+    
+    mutating func play(){
+        // TODO: 경과시간 체크
+        while(cube.continueToPlayGame){
+            let times = InputControl.getInput()
+//            cube.move(inputs: times)
+            print(times!)
+        }
+        // TODO: 모든 면을 맞출 시 축하 메시지
+    }
+}
+
+var cubeGame = CubeGame()
+cubeGame.play()
